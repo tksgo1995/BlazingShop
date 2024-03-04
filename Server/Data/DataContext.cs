fae6e97a9dbb3b0d20d8c02e5ab653079cd1e88b
산해 강 <koreaksh1995@naver.com>
2024-03-04 16:55:18 +0900

    ProductDetails Edition SelectBox 추가

--- Server/Data/DataContext.cs
+++ Server/Data/DataContext.cs
@@ -14,6 +14,7 @@ namespace BlazingShop.Server.Data
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+		public DbSet<Edition> Editions { get; set; }
 
 		protected override void OnModelCreating(ModelBuilder modelBuilder)
 		{
@@ -21,7 +22,7 @@ namespace BlazingShop.Server.Data
 				new Category{Id = 1, Name = "Books", Url = "books", Icon = "book"},
 				new Category{Id = 2, Name = "Electronics", Url = "electronics", Icon = "camera-slr"},
 				new Category{Id = 3, Name = "Video Games", Url = "video-games", Icon = "aperture"}
-			);
+				);
 
 			modelBuilder.Entity<Product>().HasData(
 				new Product
@@ -33,7 +34,7 @@ namespace BlazingShop.Server.Data
 					Image = "https://upload.wikimedia.org/wikipedia/en/b/bd/H2G2_UK_front_cover.jpg",
 					DateCreated = new DateTime(2021, 1, 1),
 					Price = 9.99m,
-					OriginalPrice = 19.99m
+					OriginalPrice = 19.99m,
 				},
 				new Product
 				{
@@ -121,7 +122,28 @@ namespace BlazingShop.Server.Data
 					DateCreated = new DateTime(2021, 1, 1),
 					Price = 14.99m
 				}
-			);
+				);
+
+			modelBuilder.Entity<Edition>().HasData(
+				new Edition { Id = 1, Name = "Paperback" },
+				new Edition { Id = 2, Name = "E-Book" },
+				new Edition { Id = 3, Name = "Audiobook" },
+				new Edition { Id = 4, Name = "PC" },
+				new Edition { Id = 5, Name = "PlayStation" },
+				new Edition { Id = 6, Name = "Xbox" }
+				);
+
+			modelBuilder.SharedTypeEntity<Dictionary<string, object>>("EditionProduct")
+				.HasData(
+					new { EditionsId = 1, ProductsId = 1 },
+					new { EditionsId = 2, ProductsId = 1 },
+					new { EditionsId = 3, ProductsId = 1 },
+					new { EditionsId = 1, ProductsId = 2 },
+					new { EditionsId = 2, ProductsId = 2 },
+					new { EditionsId = 4, ProductsId = 7 },
+					new { EditionsId = 5, ProductsId = 7 },
+					new { EditionsId = 6, ProductsId = 7 }
+				);
 		}
 	}
 }
---
+++ Server/Migrations/20240304063905_ProductDateFix.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlazingShop.Server.Migrations
+{
+    public partial class ProductDateFix : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateUpdated",
+                table: "Products",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateCreated",
+                table: "Products",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DateUpdated",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "DateUpdated",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "DateUpdated",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "DateUpdated",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "DateUpdated",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "DateUpdated",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "DateUpdated",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "DateUpdated",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "DateUpdated",
+                value: null);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateUpdated",
+                table: "Products",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateCreated",
+                table: "Products",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DateUpdated",
+                value: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "DateUpdated",
+                value: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "DateUpdated",
+                value: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "DateUpdated",
+                value: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "DateUpdated",
+                value: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "DateUpdated",
+                value: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "DateUpdated",
+                value: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "DateUpdated",
+                value: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "DateUpdated",
+                value: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
